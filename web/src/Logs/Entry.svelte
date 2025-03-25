9f4160d5d9f1b04566469eaace2d91b37f059319
Megakuul <linus.moser@megakuul.ch>
2025-03-25 20:10:07 +0100

    fix: update log entry collapse ux

--- web/src/Logs/Entry.svelte
+++ web/src/Logs/Entry.svelte
@@ -24,9 +24,8 @@
   let collapsed = $state(true)
 </script>
 
-<button transition:fade style="color: {color}; border-color: {color};" 
-  class="w-full flex flex-col gap-1 rounded-xl p-3 my-1.5 border-2 transition-all duration-700 overflow-hidden {collapsed ? "h-12" : "h-24"}"
-  onclick={() => collapsed = !collapsed}>
+<div transition:fade style="color: {color}; border-color: {color};" 
+  class="w-full flex flex-col gap-1 rounded-xl p-3 my-1.5 border-2 transition-all duration-700 overflow-hidden {collapsed ? "h-14" : "h-30"}">
   <div class="flex flex-row items-center gap-2">
     <span title="{time.toString()}" class="font-bold">
       {`${time.getHours().toString().padStart(2, '0')}:${time.getMinutes().toString().padStart(2, '0')}`}
@@ -35,6 +34,11 @@
     {#if collapsed}
       <span transition:fade class="text-nowrap overflow-hidden">{message}</span>
     {/if}
+    <button title="open" aria-label="open"
+      class="ml-auto p-1 rounded-md hover:bg-slate-700/20 transition-all duration-700" 
+      onclick={() => collapsed = !collapsed} >
+      <svg style="rotate: {collapsed ? "180deg" : "0deg"};" class="transition-all duration-700" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path stroke-dasharray="16" stroke-dashoffset="16" d="M12 19l0 -13.5"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.2s" values="16;0"/></path><path stroke-dasharray="10" stroke-dashoffset="10" d="M12 5l5 5M12 5l-5 5"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.2s" dur="0.2s" values="10;0"/></path></g></svg>
+    </button>
   </div>
   <p class="text-start text-wrap text-sm text-gray-200/80">
     {message}
@@ -43,4 +47,4 @@
     <span class="text-xs text-gray-700 underline">{file}:{line}</span>
     <span class="text-xs text-gray-700 underline">{func}</span>
   </div>
-</button>
+</div>
